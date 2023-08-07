cd05ad2c44f30a2427eb09c813740565a7f62431
Marcos PC Casa <marcospastora@gmail.com>
2023-08-07 20:52:15 -0300

    initial commit

---
+++ Models/Filme.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    public string Titulo {get; set;}
+    public string Genero {get; set;}
+    public int Duracao { get; set; }
+}
